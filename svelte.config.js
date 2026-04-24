f9ea466e8ea9e9632f54c53aa3bcf3f8fbc69d01
Szymon Mielecki <szymon.mielecki@gmail.com>
2026-04-24 11:53:30 +0200

    fix: Deployment issues

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
-import adapter from '@sveltejs/adapter-node';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const adapter =
+	process.env.VERCEL || process.env.ADAPTER === 'vercel'
+		? adapterVercel()
+		: adapterNode({ out: 'build' });
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({ out: 'build' }),
+		adapter,
 		files: {
 			serviceWorker: 'src/service-worker'
 		},
